e68a58eeb5d6c1975ef4c028c7e07e9a69b2927b
jubardt <dominiquejubard@gmail.com>
2022-02-12 16:10:09 +0100

    maj avec cors

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class calculator(Resource):
